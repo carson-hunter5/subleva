f96cedab7c3e02e8212f22116ed812b8bddfec68
chunter320 <cphunter320@gmail.com>
2024-06-09 02:01:42 +0200

    fixing the ml model.py file

---
+++ api/backend/ml_models/model02.py
@@ -0,0 +1,8 @@
+import requests
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from scipy.stats import randint
