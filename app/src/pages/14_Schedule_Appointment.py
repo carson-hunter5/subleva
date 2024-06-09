a2e76a4edbc611a1007159d935f1d7d3e795b9bb
Ivionna Jordan <jordan.iv@northeastern.edu>
2024-06-09 10:01:02 +0200

    fixing schedule appointment.py

--- app/src/pages/14_Schedule_Appointment.py
+++ app/src/pages/14_Schedule_Appointment.py
@@ -13,24 +13,49 @@ SideBarLinks()
 
 # Create a new appointment
 
-st.write("**Schedule a New Appointment:**")
-volunteerID = st.text_input("volunteerID")
-date = st.date_input("Event Date", value=datetime.date.today())
-appointmentID = st.text_input("appointmentID")
-migrantID = st.session_state["id"]
+# add columns to the volunteer table for each day of the week
+# randomly assign volunteers to days
+# when a migrant wants to make an appt for a particular day, randomly assign them 
+#      to one of the volunteers on that day. 
 
+# appts_on_weekday = {} 
+# data = requests.get('http://api:4000/m/migrant').json()
+weekday = st.selectbox(label = "Select a Weekday to View", options = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")) 
+if st.button("Pick Weekday"):
+       response = requests.get(f'http://api:4000/m/migrant/show_appt/{weekday}').json()
+       st.dataframe(response)
+
+
+""""
+if isinstance(data, list) and all(isinstance(item, dict) for item in data):
+    weekdays_list = [item['weekday'] for item in data if 'weekday' in item]
+
+st.subheader("**Schedule a New Appointment**", divider='green')
+
+if weekdays_list:
+    selected_day = st.selectbox("Select a Day", options=weekdays_list)
+
+if isinstance(data, list) and all(isinstance(item, dict) for item in data):
+        
+        available_appointments = [item['appointmentID'] for item in data if 'appointmentID' in item]
+
+        if available_appointments:
+            selected_appointment = st.selectbox("Select an Appointment Slot", options=available_appointments)
+
+            migrantID = st.session_state.get("id")
+"""
 if st.button("Submit"):
-    if volunteerID and date and appointmentID and migrantID:
-        post_data = {
-            "volunteerID" : volunteerID,
-            "appointmentID" : appointmentID,
-            "date" : date,
-            "migrantID" : migrantID
-        }
-        response = requests.post("http://api:4000/m/migrant/",json=post_data)
+                if selected_appointment and migrantID:
+                    post_data = {
+                        "appointmentID": selected_appointment,
+                        "migrantID": migrantID,
+                        "day": selected_day
+                    }
+
+response = requests.post("http://api:4000/m/migrant/",json=post_data)
 
 
 if st.button('Back', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/10_Migrant_Home.py')
+  st.switch_page('pages/11_Appointments.py')
